f497b6093574a7acae7f58024abf9244215b5f49
ozz_silva <osvaldosilva5@outlook.com>
2024-11-24 10:04:29 -0600

    Bux fix for DestroySelfOnContact

--- Assets/Scripts/UnityMultiplayer/Utils/DestroySelfOnContact.cs
+++ Assets/Scripts/UnityMultiplayer/Utils/DestroySelfOnContact.cs
@@ -7,16 +7,21 @@ public class DestroySelfOnContact : MonoBehaviour
     [SerializeField] private Projectile _projectile;
     private void OnTriggerEnter2D(Collider2D other)
     {
+        
         //Check if its a team game
         if (_projectile.TeamIndex != -1)
         {
-            if (other.TryGetComponent<TankPlayer>(out TankPlayer player))
+            if (other.attachedRigidbody != null)
             {
-                if (player.TeamIndex.Value == _projectile.TeamIndex)
+                if (other.attachedRigidbody.TryGetComponent<TankPlayer>(out TankPlayer player))
                 {
-                    return;
+                    if (player.TeamIndex.Value == _projectile.TeamIndex)
+                    {
+                        return;
+                    }
                 }
             }
+            
         }
         
         Destroy(gameObject);
